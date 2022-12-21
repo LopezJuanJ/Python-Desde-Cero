3f3e06757bd1d1f7ca1562299d613d1aee9bba32
LopezJuanJ <juanjavierlopezgarrido@gmail.com>
2022-12-21 20:44:23 +0100

    Ejercicio1

---
+++ Punto-2/Ejercicios/Ejercicio1/Ejercicio1.py
@@ -0,0 +1,25 @@
+#Definimos variables
+numero = 5
+texto = "hola"
+booleano= True
+
+#Mostrar por pantalla las variables
+print("La primera variables es ",  numero)
+print("La segunda variables es ", texto)
+print("La tercera variables es ", booleano)
+
+#Saber el tipo de las variables
+type(numero)
+type(texto)
+type(booleano)
+
+#Convetir una variable a otro tipo
+
+#Numero a Str
+numero_como_texto = str(numero)
+
+#Texto a Numero
+texto_como_numero = int(texto)
+
+#Boleano a Entero
+booleano_como_numero = int(booleano)
