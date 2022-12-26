e6e3318c71cfa8be2b24700f613512f7cb061785
LopezJuanJ <juanjavierlopegarrido@gmail.com>
2022-12-26 21:10:01 +0100

    Media

--- Punto3/Ejercicios/Ejercicio3/Ejercicio3_3.py
+++ Punto3/Ejercicios/Ejercicio3/Ejercicio3_3.py
@@ -1,5 +1,12 @@
 def odenar_lista(numero1, numero2, numero3, numero4, numero5):
-    lista = [numero2, numero2, numero3, numero4, numero5]
+    lista = [numero1, numero2, numero3, numero4, numero5]
     print(lista)
     lista.sort()
     return lista
+
+def calcular_media(numero1, numero2, numero3, numero4, numero5):
+    lista = [numero1, numero2, numero3, numero4, numero5]
+    longitud = len(lista)
+    suma = sum(lista)
+    media = suma / longitud
+    return media
--- Punto3/Ejercicios/Ejercicio3/Main3_3.py
+++ Punto3/Ejercicios/Ejercicio3/Main3_3.py
@@ -1,3 +1,4 @@
-import Ejercicio3_3 as ob1
+import Ejercicio3_3 as estadisticas
 
-print(ob1.odenar_lista(2, 3, 23, 5, 2))
+print(estadisticas.odenar_lista(2, 3, 23, 5, 2))
+print(estadisticas.calcular_media(5, 2, 6, 3, 2))
