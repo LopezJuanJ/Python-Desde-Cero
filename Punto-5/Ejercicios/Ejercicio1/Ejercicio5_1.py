395685a4de3d485f76f45eafbc37767a32678a31
LopezJuanJ <juanjavierlopegarrido@gmail.com>
2022-12-31 01:08:26 +0100

    Medio Ejercicio5_1

--- Punto-5/Ejercicios/Ejercicio1/Ejercicio5_1.py
+++ Punto-5/Ejercicios/Ejercicio1/Ejercicio5_1.py
@@ -1,8 +1,20 @@
 class Gestion:
-    def __init__(self, nombre, fichero, modo):
+    def __init__(self, nombre, modo):
         self.nombre = nombre
-        self.fichero = None
         self.modo = modo
+
+    def abrir(self):
+        f = open(self.nombre, self.modo)
+        return f
+
+    def linea_a_linea(self):
+        archivo = open(self.nombre, self.modo)
+        for linea in archivo:
+            print(linea)
+
+    def cerrar(self):
+        archivo = open(self.nombre, self.modo)
+        archivo.close() 
 
     
     
--- Punto-5/Ejercicios/Ejercicio1/Main5_1.py
+++ Punto-5/Ejercicios/Ejercicio1/Main5_1.py
@@ -1,5 +1,7 @@
 import Ejercicio5_1
 
-p1 = Ejercicio5_1.Gestion("datos.txt", None, "r")
+p1 = Ejercicio5_1.Gestion("datos.txt", "r")
 
 p1.abrir()
+p1.linea_a_linea()
+p1.cerrar()
