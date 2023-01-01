1768783053e4b7471877cb047398dd433dd09115
LopezJuanJ <juanjavierlopegarrido@gmail.com>
2023-01-01 18:28:09 +0100

    Estetica

--- Punto-6/Ejercicios/Ejercicio1/Ejercicio6_1.py
+++ Punto-6/Ejercicios/Ejercicio1/Ejercicio6_1.py
@@ -4,4 +4,4 @@ class Remplazar:
         texto = "Hoy es lunes 20 de marzo de 2020. Mañana será martes 21 de marzo de 2020."
         numeros = re.findall(r'\d+', texto)
         numeros = [int(x) for x in numeros]
-        print(numeros)  # El resultado debería ser: [20, 21, 2020, 2020]
+        print(numeros) 
